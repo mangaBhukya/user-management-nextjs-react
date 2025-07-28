5584d7caefb3d6099a9969bdb8326f72d4d7446e
Manga <mangab617@gmail.com>
2025-07-28 14:26:31 +0530

    Create SignUp page with form validation and localStorage save

---
+++ src/app/signUp/page.tsx
@@ -0,0 +1,127 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Navbar from "@/components/Navbar";
+
+type FormErrors = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+
+export default function SignUpPage() {
+
+  const router = useRouter();
+  const [form, setForm] = useState({ name: "", email: "", password: "" });
+  const [errors, setErrors] = useState({ name: "", email: "", password: "" });
+
+  const validate = () => {
+    const tempErrors: FormErrors = {
+      name: "",
+      email: "",
+      password: ""
+    };
+    tempErrors.name = form.name ? "" : "Name is required";
+    tempErrors.email = /^\S+@\S+\.\S+$/.test(form.email) ? "" : "Email is invalid";
+    tempErrors.password = form.password.length >= 6 ? "" : "Minimum 6 characters required";
+
+    setErrors(tempErrors);
+    return Object.values(tempErrors).every(x => x === "");
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    console.log('handle----', e)
+    e.preventDefault();
+   if (validate()) {
+      localStorage.setItem("userData", JSON.stringify({
+        name: form.name,
+        email: form.email
+      }));
+      alert("Signed up successfully!");
+      router.push("/");
+    }
+  }
+
+  return (
+    <>
+     <Navbar />
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+            Create Your Account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                Full Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="name"
+                  required
+                  autoComplete="name"
+                  onChange={(e) => setForm({ ...form, name: e.target.value })}
+                  value={form.name}
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  value={form.email}
+                  onChange={(e) => setForm({ ...form, email: e.target.value })}
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  value={form.password}
+                  onChange={(e) => setForm({ ...form, password: e.target.value })}
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            {errors.password && <p className="text-red-500 text-sm">{errors.password}</p>}
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign Up
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
